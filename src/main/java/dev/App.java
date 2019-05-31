dee89ca8013d58b2db99d7caef06c5ab8144b385
Glen OLLIVIER <glen.ollivier@gmail.com>
2019-05-31 15:25:05 +0200

    Ajout fichier properties

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,16 +1,21 @@
 package dev;
 
 import java.io.IOException;
+import java.util.ResourceBundle;
 
 import com.github.lalyos.jfiglet.FigletFont;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args ) throws IOException {
-        String titre = FigletFont.convertOneLine("Premier projet Maven");
-        System.out.println( titre );
-    }
+public class App {
+	public static void main(String[] args) throws IOException {
+		String titre = ResourceBundle.getBundle("application").getString("titre");
+		titre = FigletFont.convertOneLine(titre);
+		System.out.println(titre);
+
+		String environnement = ResourceBundle.getBundle("application").getString("environnement");
+		System.out.println("Environnement : " + environnement);
+	}
 }
